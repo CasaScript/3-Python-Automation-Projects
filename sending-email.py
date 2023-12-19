f7b429bb6e6655cc314a7cd2488af94b156f1b6f
CasaScript <132998630+CasaScript@users.noreply.github.com>
2023-12-19 10:51:46 +0100

    sending-email.py

--- sending-email.py
+++ sending-email.py
@@ -1,35 +1,57 @@
 import smtplib
 import ssl
 from email.message import EmailMessage
+import os
+import sys
 
-subject = "Email From Python"
-body = "This is a test email form Python!"
-sender_email = "coderacerlivestreamapp@gmail.com"
-receiver_email = "coderacerlivestreamapp@gmail.com"
-password = input("Enter a password: ")
+def send_email(subject, body, sender_email, receiver_email, password):
+    """
+    Sends an email from the sender to the receiver.
 
-message = EmailMessage()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
+    Parameters:
+    subject (str): The subject of the email.
+    body (str): The body content of the email.
+    sender_email (str): The sender's email address.
+    receiver_email (str): The receiver's email address.
+    password (str): The password for the sender's email account.
+    """
+    message = EmailMessage()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message.set_content(body)
 
-html = f"""
-<html>
-    <body>
-        <h1>{subject}</h1>
-        <p>{body}</p>
-    </body>
-</html>
-"""
+    context = ssl.create_default_context()
 
-message.add_alternative(html, subtype="html")
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        print("Email sent successfully!")
+    except smtplib.SMTPAuthenticationError:
+        print("Authentication failed. Check your email and password.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-context = ssl.create_default_context()
+def main():
+    """
+    Main function to set up and send an email.
+    """
+    subject = "Email From Assistant"
+    body = "This is a test email from Robot!"
 
-print("Sending Email!")
+    sender_email = "virtualstudent.x@gmail.com"
+    receiver_email = "mohamedkhaliljelassi00@gmail.com"
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.as_string())
+    # Fetching the password from an environment variable for security.
+    password = os.getenv("EMAIL_PASSWORD")
 
-print("Success")
+    if not password:
+        print("No password provided. Please set the EMAIL_PASSWORD environment variable.")
+        sys.exit(1)
+
+    send_email(subject, body, sender_email, receiver_email, password)
+
+if __name__ == "__main__":
+    main()
+
